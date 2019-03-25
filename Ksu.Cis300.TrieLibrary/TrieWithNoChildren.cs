277e26285bcfda0c6849b8c1f1a8b44b11a8f1ba
Agustin Rodriguez <agustinr@ksu.edu>
2019-03-25 12:06:18 -0500

    lab 22

---
+++ Ksu.Cis300.TrieLibrary/TrieWithNoChildren.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ksu.Cis300.TrieLibrary
+{
+
+    public class TrieWithNoChildren : ITrie
+    {
+        /// <summary>
+        /// indicates whether the empty string is stored in the 
+        /// trie rooted at this node
+        /// </summary>
+        private bool _isEmpty = false;
+        /// <summary>
+        /// adds string to trie
+        /// </summary>
+        /// <param name="s"></param>string to be added
+        /// <returns></returns>ITrie with new string
+        public ITrie Add(string s)
+        {
+            if (s == "")
+            {
+                _isEmpty = true;
+                return this;
+            }
+            else
+            {
+                return new TrieWithOneChild(s, _isEmpty);
+            }
+        }
+        /// <summary>
+        /// checks to see if trie contains s
+        /// </summary>
+        /// <param name="s"></param>string we are looking for
+        /// <returns></returns> returns true or false
+        public bool Contains(string s)
+        {
+            if (s == "")
+            {
+                return _isEmpty;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Ksu.Cis300.TrieLibrary/TrieWithOneChild.cs
@@ -0,0 +1,96 @@
+﻿/*
+ *Agustin Rodriguez
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ksu.Cis300.TrieLibrary
+{
+    public class TrieWithOneChild : ITrie
+    {
+        /// <summary>
+        /// field to check whether the trie contains the 
+        /// empty string
+        /// </summary>
+        private bool _contains;
+
+        /// <summary>
+        /// the only child
+        /// </summary>
+        private ITrie _onlyChild;
+
+        /// <summary>
+        /// gives us child's label
+        /// </summary>
+        private char _childLabel;
+
+        /// <summary>
+        /// constructor for class
+        /// </summary>
+        /// <param name="s"></param> string we are adding
+        /// <param name="b"></param>boolean whether trie contains the string
+        public TrieWithOneChild(string s, bool b)
+        {
+            if (s == "" || (s[0] < 'a' || s[0] > 'z'))
+            {
+                throw new ArgumentException();
+            }
+            _contains = b;
+            _childLabel = s[0];
+            _onlyChild = new TrieWithNoChildren().Add(s.Substring(1)); 
+            
+        }
+        /// <summary>
+        /// adds a string to trie
+        /// </summary>
+        /// <param name="s"></param> string we want to add
+        /// <returns></returns> returns itrie with string
+        public ITrie Add(string s)
+        {
+            if (s == "")
+            {
+                _contains = true;
+                return this;
+            }
+            else if (s[0] < 'a' || s[0] > 'z')
+            {
+                throw new ArgumentException();
+
+            }
+            else if (s[0] == _childLabel)
+            {
+                _onlyChild = _onlyChild.Add(s.Substring(1));
+                return this;
+            }
+            else
+            {
+                return new TrieWithManyChildren(s, _contains, _childLabel, _onlyChild);
+            }
+            
+           
+        }
+        /// <summary>
+        /// checks to see if trie contains the string
+        /// </summary>
+        /// <param name="s"></param>string we are looking for
+        /// <returns></returns> returns bool if true or not
+        public bool Contains(string s)
+        {
+            if (s == "")
+            {
+                return _contains;
+            }
+            if (s[0] == _childLabel)
+            {
+                return _onlyChild.Contains(s.Substring(1));
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
